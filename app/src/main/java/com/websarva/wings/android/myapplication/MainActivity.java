e4c7b4c58dfd6986a1438088ce94c4c939f6252d
yanoatusi <rG734jQyup2A@outlook.jp>
2020-03-11 11:49:44 +0900

    gps1

--- app/src/main/java/com/websarva/wings/android/myapplication/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/myapplication/MainActivity.java
@@ -1,14 +1,85 @@
 package com.websarva.wings.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // LocationClientクラスのインスタンスを生成
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // 位置情報取得開始
+        startUpdateLocation();
+    }
+
+    /**
+     * 位置情報取得開始メソッド
+     */
+    private void startUpdateLocation() {
+        // 位置情報取得権限の確認
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 権限がない場合、許可ダイアログ表示
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, 2000);
+            return;
+        }
+
+        // 位置情報の取得方法を設定
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);       // 位置情報更新間隔の希望
+        locationRequest.setFastestInterval(5000); // 位置情報更新間隔の最速値
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); // この位置情報要求の優先度
+
+        fusedLocationClient.requestLocationUpdates(locationRequest,  new MyLocationCallback(), null);
+    }
+    /**
+     * 位置情報受取コールバッククラス
+     */
+    private class MyLocationCallback extends LocationCallback {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult == null) {
+                return;
+            }
+            // 現在値を取得
+            Location location = locationResult.getLastLocation();
+            // 画面に表示
+            TextView view = findViewById(R.id.text_view);
+            view.setText("緯度:" + location.getLatitude() + " 経度:" + location.getLongitude());
+        };
+    }
+
+    /**
+     * 許可ダイアログの結果受取
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 2000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // 位置情報取得開始
+            startUpdateLocation();
+        }
     }
 }
